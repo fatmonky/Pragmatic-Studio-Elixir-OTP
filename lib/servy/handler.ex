2cf3f49b9dca9d2005947c4f99690e71289a1490
PJ Teh <peijing.teh.dev@gmail.com>
2025-06-26 21:29:55 +0800

    ch 13: completed

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -68,12 +68,14 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%Conv{method: "GET", path: "/about"} = conv) do
-    %{conv | status: 200, resp_body: "contents of a file"}
-  end
-
   def route(%Conv{method: "DELETE", path: "/bears" <> _id} = conv) do
     %{conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
+  end
+
+  # name=Baloo&type=Brown
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    params = %{"name" => "Baloo", "type" => "Brown"}
+    %{conv | status: 201, resp_body: "Created a #{params["type"]} bear named #{params["name"]}!"}
   end
 
   def route(%Conv{path: path} = conv) do
@@ -157,6 +159,21 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
+"""
+
+response = Servy.Handler.handler(request)
+
+IO.puts(response)
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
 """
 
 response = Servy.Handler.handler(request)
--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -2,12 +2,17 @@ defmodule Servy.Parser do
   alias Servy.Conv, as: Conv
 
   def parse(request) do
-    [method, route, _] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
+    [top, params_string] = String.split(request, "\n\n")
+    [request_line | header_lines] = String.split(top, "\n")
 
-    %Conv{method: method, path: route}
+    [method, route, _] = String.split(request_line, " ")
+
+    params = parse_params(params_string)
+
+    %Conv{method: method, path: route, params: params}
+  end
+
+  def parse_params(params_string) do
+    params_string |> String.trim() |> URI.decode_query()
   end
 end
