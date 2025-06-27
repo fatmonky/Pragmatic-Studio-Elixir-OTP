b02061e1c4286e572018c67ba54ece0c8b94ae0d
peijing.teh@gmail.com <peijing.teh@gmail.com>
2025-06-27 13:30:06 +0800

    ch 14: completed triple function exercise.

--- examples/recurse.ex
+++ examples/recurse.ex
@@ -1,13 +1,31 @@
 defmodule Recurse do
   def loopy([head | tail], aggregator) do
-    IO.puts("Head: #{head}, Tail: #{tail}")
+    # IO.puts("Head: #{head}, Tail: #{tail}")
     aggregator = head + aggregator
     loopy(tail, aggregator)
   end
 
   def loopy([], aggregator), do: aggregator
+
+  # Ch 14 ex 2
+
+  def triple(list) do
+    triple(list, [])
+  end
+
+  def triple([head | tail], _new_list) do
+    # take each head multiply by 3
+    # add to new list with each recursion
+    [head * 3 | triple(tail, [])]
+    # triple(tail, new_list)
+  end
+
+  def triple([], new_list), do: new_list
 end
 
 nums = [1, 2, 3, 4, 5]
 sum = Recurse.loopy(nums, 0)
 IO.puts("The sum is #{sum}")
+
+new_list = Recurse.triple(nums)
+IO.inspect(new_list)
