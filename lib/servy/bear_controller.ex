13c5e784a441cb28af1523251bf98b170b32cb04
PJ Teh <peijing.teh.dev@gmail.com>
2025-06-28 11:56:46 +0800

    ch 15 2nd video: before refactoring anonymous functions

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -2,15 +2,20 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
 
   def index(conv) do
-    bears = Wildthings.list_bears()
-    # TODO: transform bears to an HTML list
-    # previously:
-    # %{conv | status: 200, resp_body: "Pandas, Black, Sun"}
-    %{conv | status: 200, resp_body: "<ul><li>Name</li><ul>"}
+    items =
+      Wildthings.list_bears()
+      |> Enum.filter(fn b -> b.type == "Grizzly" end)
+      |> Enum.sort(fn b1, b2 -> b1.name <= b2.name end)
+      |> Enum.map(fn b -> "<li>#{b.name} - #{b.type}</li>" end)
+      |> Enum.join()
+
+    %{conv | status: 200, resp_body: "<ul>#{items}</ul>"}
   end
 
   def show(conv, %{"id" => id}) do
-    %{conv | status: 200, resp_body: "Bear #{id}"}
+    bear = Wildthings.get_bear(id)
+
+    %{conv | status: 200, resp_body: "<h1>Bear #{bear.id}: #{bear.name}</h1>"}
   end
 
   def create(conv, %{"name" => name, "type" => type}) do
