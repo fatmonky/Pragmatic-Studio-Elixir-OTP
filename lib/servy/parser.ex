de7850c34aca97c66554f68fbfade7286828ec16
peijing.teh@gmail.com <peijing.teh@gmail.com>
2025-06-28 17:02:22 +0800

    ch 15 exercise Reduce Headers completed

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -6,19 +6,28 @@ defmodule Servy.Parser do
     [request_line | header_lines] = String.split(top, "\n")
 
     [method, route, _] = String.split(request_line, " ")
-    headers = parse_headers(header_lines, %{})
+    headers = parse_headers(header_lines)
     params = parse_params(headers["Content-Type"], params_string)
     %Conv{method: method, path: route, params: params, headers: headers}
   end
 
-  def parse_headers([head | tail], header) do
-    split_string = String.split(head, ": ")
-    [key, value] = split_string
-    header = Map.put(header, key, value)
-    parse_headers(tail, header)
-  end
+  # def parse_headers([head | tail], header) do
+  #   split_string = String.split(head, ": ")
+  #   [key, value] = split_string
+  #   header = Map.put(header, key, value)
+  #   parse_headers(tail, header)
+  # end
 
-  def parse_headers([], header), do: header
+  # def parse_headers([], header), do: header
+
+  # ch 15 reduce headers exercise
+  def parse_headers(header_lines) do
+    Enum.reduce(header_lines, %{}, fn header, parsed_headers ->
+      split_string = String.split(header, ": ")
+      [key, value] = split_string
+      header = Map.put(parsed_headers, key, value)
+    end)
+  end
 
   def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim() |> URI.decode_query()
