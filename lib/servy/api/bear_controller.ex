922d4f9b3eec0fa9f2d16f06bb1859ec6a235fd7
PJ Teh <peijing.teh.dev@gmail.com>
2025-07-08 10:37:14 +0800

    ch 19 Ex 1 Rendering JSON: add put_resp_content_type

--- lib/servy/api/bear_controller.ex
+++ lib/servy/api/bear_controller.ex
@@ -1,6 +1,12 @@
 defmodule Servy.Api.BearController do
   def index(conv) do
     json = Servy.Wildthings.list_bears() |> Poison.encode!()
-    %{conv | status: 200, resp_body: json, resp_headers: %{"Content-Type" => "application/json"}}
+    conv = put_resp_content_type(conv, "application/json")
+    %{conv | status: 200, resp_body: json}
+  end
+
+  def put_resp_content_type(conv, content_type) do
+    new_headers = Map.put(conv.resp_headers, "Content-Type", content_type)
+    conv = %{conv | resp_headers: new_headers}
   end
 end
