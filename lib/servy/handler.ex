16a36d610ffaa473bf813816d7f8f02372d0623a
PJ Teh <peijing.teh.dev@gmail.com>
2025-07-08 16:27:59 +0800

    ch19 Ex 3: model answer

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -64,9 +64,16 @@ defmodule Servy.Handler do
     |> Path.join("#{name}.md")
     |> File.read()
     |> handle_file(conv)
+    |> markdown_to_html(conv)
+
     # model answer: declare markdown_to_html function, instead of calling Earmark.as_html directly
-    |> Earmark.as_html!()
   end
+
+  def markdown_to_html(%Conv{status: 200} = conv) do
+    %{conv | resp_body: Earmark.as_html!(conv.resp_body)}
+  end
+
+  def markdown_to_html(%Conv{} = conv), do: conv
 
   # function clause approach
   def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
