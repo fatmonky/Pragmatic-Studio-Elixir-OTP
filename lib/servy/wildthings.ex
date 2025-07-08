0741fed772709d15285261ad973f6471ee58f102
PJ Teh <peijing.teh.dev@gmail.com>
2025-07-08 15:39:35 +0800

    * db/bears.json: add json & attempt at list_bears with Poison.decode!

--- lib/servy/wildthings.ex
+++ lib/servy/wildthings.ex
@@ -2,19 +2,27 @@ defmodule Servy.Wildthings do
   alias Servy.Bear
 
   def list_bears do
-    [
-      %Bear{id: 1, name: "Teddy", type: "Brown", hibernating: true},
-      %Bear{id: 2, name: "Smokey", type: "Black"},
-      %Bear{id: 3, name: "Paddington", type: "Brown"},
-      %Bear{id: 4, name: "Scarface", type: "Grizzly", hibernating: true},
-      %Bear{id: 5, name: "Snow", type: "Polar"},
-      %Bear{id: 6, name: "Brutus", type: "Grizzly"},
-      %Bear{id: 7, name: "Rosie", type: "Black", hibernating: true},
-      %Bear{id: 8, name: "Roscoe", type: "Panda"},
-      %Bear{id: 9, name: "Iceman", type: "Polar", hibernating: true},
-      %Bear{id: 10, name: "Kenai", type: "Grizzly"}
-    ]
+    json = File.read("../../db/bears.json")
+    IO.puts(json)
+    bear_list = Poison.decode!(json, as: %{"bears" => [%Bear{}]})
+    IO.puts(bear_list)
+    bear_list
   end
+
+  # def list_bears do
+  #   [
+  #     %Bear{id: 1, name: "Teddy", type: "Brown", hibernating: true},
+  #     %Bear{id: 2, name: "Smokey", type: "Black"},
+  #     %Bear{id: 3, name: "Paddington", type: "Brown"},
+  #     %Bear{id: 4, name: "Scarface", type: "Grizzly", hibernating: true},
+  #     %Bear{id: 5, name: "Snow", type: "Polar"},
+  #     %Bear{id: 6, name: "Brutus", type: "Grizzly"},
+  #     %Bear{id: 7, name: "Rosie", type: "Black", hibernating: true},
+  #     %Bear{id: 8, name: "Roscoe", type: "Panda"},
+  #     %Bear{id: 9, name: "Iceman", type: "Polar", hibernating: true},
+  #     %Bear{id: 10, name: "Kenai", type: "Grizzly"}
+  #   ]
+  # end
 
   def get_bear(id) when is_integer(id) do
     Enum.find(list_bears(), fn b -> b.id == id end)
