0f0fd7220b64e0d5a920a4bc8f1102e185f24023
PJ Teh <peijing.teh.dev@gmail.com>
2025-07-09 20:45:22 +0800

    ch22 Ex1 send receive msg: complete!

--- exercises/ch22/power_napper.ex
+++ exercises/ch22/power_napper.ex
@@ -1,10 +1,28 @@
-defmodule Nap do
-  parent = self()
-  power_nap = fn ->
-    time = :rand.uniform(10_000)
-    :timer.sleep(time)
-    time
+parent = self()
+
+power_nap = fn ->
+  time = :rand.uniform(10_000)
+  :timer.sleep(time)
+  time
+end
+
+spawn(fn ->
+  time = power_nap.()
+  send(parent, {:slept, time})
+end)
+
+naptime =
+  receive do
+    {:slept, time} -> time
   end
 
-  spawn(power_nap; send(parent, ))
-end
+IO.puts("Slept #{naptime}ms")
+
+# model answer:
+# parent = self()
+
+# spawn(fn -> send(parent, {:slept, power_nap.()}) end)
+
+# receive do
+#   {:slept, time} -> IO.puts "Slept #{time} ms"
+# end
