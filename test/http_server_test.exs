d722bdd66a892dfaaf216b809a305ecfd401e44b
PJ Teh <peijing.teh.dev@gmail.com>
2025-07-10 16:23:40 +0800

    ch 22 Ex using HTTPoison for client in HTTPservertest

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -26,4 +26,41 @@ defmodule Servy.HttpServerTest do
            Bears, Lions, Tigers, Tapirs
            """
   end
+
+  test "GET /bears" do
+    {:ok, response} = HTTPoison.get("http://localhost:4000/bears")
+
+    expected_response = """
+    <h1>All The Bears!</h1>
+
+    <ul>
+      <li>Brutus - Grizzly</li>
+      <li>Iceman - Polar</li>
+      <li>Kenai - Grizzly</li>
+      <li>Paddington - Brown</li>
+      <li>Roscoe - Panda</li>
+      <li>Rosie - Black</li>
+      <li>Scarface - Grizzly</li>
+      <li>Smokey - Black</li>
+      <li>Snow - Polar</li>
+      <li>Teddy - Brown</li>
+    </ul>
+    """
+
+    assert remove_whitespace(response.body) == remove_whitespace(expected_response)
+  end
+
+  # model answer
+  # test "accepts a request on a socket and sends back a response" do
+  #   spawn(HttpServer, :start, [4000])
+
+  #   {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+
+  #   assert response.status_code == 200
+  #   assert response.body == "Bears, Lions, Tigers"
+  # end
+
+  defp remove_whitespace(text) do
+    String.replace(text, ~r{\s}, "")
+  end
 end
