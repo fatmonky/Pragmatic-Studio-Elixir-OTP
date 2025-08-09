1b92de6b4df0561e4242430dd6acb4257446ec54
PJ Teh <peijing.teh.dev@gmail.com>
2025-08-09 21:20:19 +0800

    ch 24 ex 1: unit test for PledgeServer

---
+++ test/pledge_server_test.exs
@@ -0,0 +1,19 @@
+defmodule Servy.PledgeServerTest do
+  use ExUnit.Case
+
+  alias Servy.PledgeServer
+
+  test "server caches only 3 most recent pledges, and totals amounts" do
+    pid = Servy.PledgeServer.start()
+    PledgeServer.create_pledge("larry", 10)
+    PledgeServer.create_pledge("moe", 20)
+    PledgeServer.create_pledge("curly", 30)
+    PledgeServer.create_pledge("daisy", 40)
+    PledgeServer.create_pledge("grace", 50)
+
+    no_recent_pledges = PledgeServer.recent_pledges() |> Enum.count()
+    totals = 30 + 40 + 50
+    assert no_recent_pledges == 3
+    assert PledgeServer.total_pledges() == totals
+  end
+end
