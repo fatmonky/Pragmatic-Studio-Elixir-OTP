4f8d5cf1d9dc6133032b55d15827f7fac62cdedd
PJ Teh <peijing.teh.dev@gmail.com>
2025-08-10 17:50:12 +0800

    ch 24 Ex - started exercise on stateful server processes.

---
+++ lib/servy/four_oh_four.ex
@@ -0,0 +1,9 @@
+defmodule Servy.FourOhFourCounter do
+  # Server Processes: start & get_counts
+  def start() do
+    # start server process
+    #
+  end
+
+  # Client processes should be bump-count & get_count
+end
