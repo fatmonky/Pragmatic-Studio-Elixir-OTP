48d42e23b22ace7f418b9ab1817009166b29170b
PJ Teh <peijing.teh.dev@gmail.com>
2025-08-31 20:53:42 +0800

    ch25 ex404 debug with chatgpt, to be as close to model answer as possible

--- lib/servy/four_oh_four.ex
+++ lib/servy/four_oh_four.ex
@@ -1,91 +1,91 @@
 defmodule Servy.GenFourOhFourCounter do
   alias Servy.FourOhFourCounter, as: Counter
 
-  def start() do
+  def start(callback_module, initial_state, name) do
     # start server process
-    pid = spawn(fn -> listen_loop() end)
-    Process.register(pid, :listen_loop)
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, name)
     pid
   end
 
-  def listen_loop(state \\ %{}) do
+  def listen_loop(state \\ %{}, callback_module) do
     # receive-do for incoming message
     receive do
       {:cast, message} ->
         case message do
           {:count, pathname} ->
             new_state = Counter.handle_cast(:count, pathname, state)
-            listen_loop(new_state)
+            listen_loop(new_state, callback_module)
 
           :clear ->
-            new_state = Counter.handle_cast(:clear)
-            listen_loop(new_state)
+            new_state = Counter.handle_cast(:clear, state)
+            listen_loop(new_state, callback_module)
         end
 
-      {:call, message} ->
+      {:call, sender, message} ->
         case message do
-          {pid, :get_count, pathname} ->
-            _count = Counter.handle_call({pid, :get_count, pathname}, state)
-            listen_loop(state)
+          {:get_count, pathname} ->
+            Counter.handle_call({:get_count, pathname}, state, sender)
+            listen_loop(state, callback_module)
 
-          {pid, :get_counts} ->
-            new_state = Counter.handle_call({pid, :get_counts}, state)
-            listen_loop(new_state)
+          :get_counts ->
+            new_state = Counter.handle_call(:get_counts, state, sender)
+            listen_loop(new_state, callback_module)
         end
 
       unexpected ->
         IO.puts("received unexpected stuff in messages! #{unexpected}")
-        listen_loop(state)
+        listen_loop(state, callback_module)
     after
       4000 ->
         IO.puts("waited 4 eternities, still nothing happened... timeout")
-        listen_loop(state)
+        listen_loop(state, callback_module)
     end
   end
 
-  def call(message) do
-    send(:listen_loop, {:call, message})
+  def call(pid, message) do
+    send(pid, {:call, self(), message})
 
     receive do
-      {:listen_loop, response} -> response
+      {:response, response} -> response
     after
       3000 -> IO.puts("no response from server")
     end
   end
 
-  def cast(message) do
-    send(:listen_loop, {:cast, message})
+  def cast(pid, message) do
+    send(pid, {:cast, message})
   end
 end
 
 defmodule Servy.FourOhFourCounter do
+  @name :four_oh_four
+
   alias Servy.GenFourOhFourCounter, as: GenCounter
   # Server Processes: start & get_counts
   def start() do
-    GenCounter.start()
+    GenCounter.start(__MODULE__, %{}, @name)
   end
 
   # Client processes should be bump-count & get_count
   def bump_count(pathname) do
-    GenCounter.cast({:count, pathname})
+    GenCounter.cast(@name, {:count, pathname})
   end
 
   def get_count(pathname) do
-    pid = self()
-    GenCounter.call({pid, :get_count, pathname})
+    GenCounter.call(@name, {:get_count, pathname})
   end
 
   def get_counts() do
-    pid = self()
-    GenCounter.call({pid, :get_counts})
+    GenCounter.call(@name, :get_counts)
   end
 
   def clear() do
-    GenCounter.cast(:clear)
+    GenCounter.cast(@name, :clear)
   end
 
   # Helpers
-  def handle_cast(:clear) do
+  def handle_cast(:clear, _state) do
     %{}
   end
 
@@ -94,14 +94,14 @@ defmodule Servy.FourOhFourCounter do
     new_state
   end
 
-  def handle_call({pid, :get_count, pathname}, state) do
+  def handle_call({:get_count, pathname}, state, sender) do
     count = Map.get(state, pathname)
-    send(pid, {:listen_loop, count})
+    send(sender, {:response, count})
     count
   end
 
-  def handle_call({pid, :get_counts}, state) do
-    send(pid, {:listen_loop, state})
+  def handle_call(:get_counts, state, sender) do
+    send(sender, {:response, state})
     state
   end
 end
