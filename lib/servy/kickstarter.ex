ff160418923a62e1062f90e39cdf060f806f6d47
PJ Teh <peijing.teh.dev@gmail.com>
2025-09-21 21:37:38 +0800

    ch28 - codealong - refactored handle_info & start_http_server funcs

--- lib/servy/kickstarter.ex
+++ lib/servy/kickstarter.ex
@@ -8,14 +8,21 @@ defmodule Servy.Kickstarter do
 
   def init(:ok) do
     Process.flag(:trap_exit, true)
+    server_pid = start_http_server()
+    {:ok, server_pid}
+  end
+
+  def handle_info({:EXIT, _pid, reason}, _state) do
+    server_pid = start_http_server()
+    {:noreply, server_pid}
+  end
+
+  def start_http_server do
     IO.puts("starting the HTTP server...")
     server_pid = spawn(Servy.HttpServer, :start, [4000])
     # links HTTP server process to Kickstart process
     Process.link(server_pid)
     Process.register(server_pid, :http_server)
-    {:ok, server_pid}
-  end
-
-  def handle_info({:EXIT, pid, :reason}, _state) do
+    server_pid
   end
 end
