29b97aad91bee85f52290292f49f92d1ec65e408
PJ Teh <peijing.teh.dev@gmail.com>
2025-09-21 21:19:49 +0800

    ch 28 - linking two processes:kickstarter with httpserver

--- lib/servy/kickstarter.ex
+++ lib/servy/kickstarter.ex
@@ -9,6 +9,8 @@ defmodule Servy.Kickstarter do
   def init(:ok) do
     IO.puts("starting the HTTP server...")
     server_pid = spawn(Servy.HttpServer, :start, [4000])
+    # links HTTP server process to Kickstart process
+    Process.link(server_pid)
     Process.register(server_pid, :http_server)
     {:ok, server_pid}
   end
